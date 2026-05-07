6f749b827bdce8c772821f745af7ca1bb1eab9c9
kryptonblade <chvk2597@gmail.com>
2026-05-07 19:25:46 +0530

    Remove submodule, treat as regular folder

---
+++ geobleu/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='geobleu',
+      version='0.5',
+      description='Implementation of GEO-BLEU and other evaluation metrics for HuMob Challenge',
+      url='https://github.com/yahoojapan/geobleu',
+      author='Toru Shimizu, LY Corporation',
+      author_email='toshimiz@lycorp.co.jp',
+      license='MIT',
+      install_requires=['numpy', 'scipy'],
+      packages=['geobleu'],
+      zip_safe=False)
